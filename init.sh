a3b50a3bdccb6819ac488e520179a862a231d045
Denis <118184940+dnekras8@users.noreply.github.com>
2023-03-08 00:50:43 +0500

    added3

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo mv /home/box/StepikNginx/web ~
+sudo mv /home/box/stepik_web_serv/web ~
 sudo ln -fs /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
